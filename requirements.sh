2bde6449c8b856fe117889e253e6bf1a64023e3e
Filip Schouwenaars <filip@datacamp.com>
2017-02-06 14:44:39 +0100

    remove hardcoded stuff

--- requirements.sh
+++ requirements.sh
@@ -1,15 +1,1 @@
 # pip installs come here.
-PYTHONWHAT_VERSION="v2.3.0"
-PYTHONBACKEND_VERSION="v0.1.7"
-
-# Install pythonwhat
-pip3 install dill==0.2.5 --target $SHARED_PYTHON_PATH --upgrade
-pip3 install ipython==4.2.1 --target $SHARED_PYTHON_PATH --upgrade
-pip3 install numpy==1.11.2 --target $SHARED_PYTHON_PATH --upgrade
-pip3 install pandas==0.19.1 --target $SHARED_PYTHON_PATH --upgrade
-pip3 install markdown2==2.3.1 --target $SHARED_PYTHON_PATH --upgrade
-pip3 install Jinja2==2.8 --target $SHARED_PYTHON_PATH --upgrade
-pip3 install git+https://github.com/datacamp/pythonwhat@$PYTHONWHAT_VERSION --target $SHARED_PYTHON_PATH --upgrade --no-dependencies
-
-# Install pythonbackend
-pip3 install git+https://$GITHUB_TOKEN@github.com/datacamp/pythonbackend.git/@$PYTHONBACKEND_VERSION --target $SHARED_PYTHON_PATH --upgrade --no-dependencies
